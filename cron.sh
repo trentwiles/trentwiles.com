003c870c404d68cd4df5559ea294a2f7d0171683
Trent Wiles <59586759+trentwiles@users.noreply.github.com>
2024-12-09 21:36:58 -0500

    call from direct location

--- cron.sh
+++ cron.sh
@@ -2,5 +2,5 @@
 echo "Script started at $(date)"
 cd /home/trent/trentwiles.com || exit
 git pull
-npx @11ty/eleventy
+/home/trent/.nvm/versions/node/v19.4.0/bin/npx @11ty/eleventy
 echo "Script ended at $(date)"
