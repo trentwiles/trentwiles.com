c0b9ad7394e0b752f5392e39273f3ca0a926ed6f
Trent Wiles <59586759+trentwiles@users.noreply.github.com>
2024-12-09 21:41:10 -0500

    crontab: tell bash where to find npx

--- cron.sh
+++ cron.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Script started at $(date)"
+# Source npx
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Continue with the rest of the script
 cd /home/trent/trentwiles.com || exit
 git pull
-/home/trent/.nvm/versions/node/v19.4.0/bin/npx @11ty/eleventy
+npx @11ty/eleventy
 echo "Script ended at $(date)"
